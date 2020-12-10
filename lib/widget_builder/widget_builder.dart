078fd62614ae949f4586d08b59f4ed2da6065905
Arenas Sicacha <j.arenas.sicacha@accenture.com>
2020-12-10 10:35:54 -0500

    widget builder library

---
+++ lib/widget_builder/widget_builder.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class WidgetBuilder extends StatefulWidget {
+  final Widget child;
+
+  WidgetBuilder({Key key, this.child}) : super(key: key);
+
+  final _WidgetBuilderState _wbs = _WidgetBuilderState();
+
+  void rebuild() {
+    _wbs.rebuildWidget();
+  }
+
+  @override
+  _WidgetBuilderState createState() => _wbs;
+}
+
+class _WidgetBuilderState extends State<WidgetBuilder> {
+  void rebuildWidget() {
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: widget.child,
+    );
+  }
+}
